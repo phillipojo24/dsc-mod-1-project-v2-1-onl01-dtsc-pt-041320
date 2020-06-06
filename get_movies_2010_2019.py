1ca6fec38163a5fa194ca819b8e563c0453e2bf5
Phillip Ojo <phillipojo24@gmail.com>
2020-06-06 02:55:24 -0400

    commit

--- get_movies_2010_2019.py
+++ get_movies_2010_2019.py
@@ -38,3 +38,16 @@ movietitleList = driver.find_element_by_id('herf')
 
     
 print('end of process')
+
+
+
+import math
+
+millnames = ['',' K',' M',' B',' T']
+
+def millify(n):
+    n = float(n)
+    millidx = max(0,min(len(millnames)-1,
+                        int(math.floor(0 if n == 0 else math.log10(abs(n))/3))))
+
+    return '{:.0f}{}'.format(n / 10**(3 * millidx), millnames[millidx])
